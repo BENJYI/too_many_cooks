1015ce9f6973de09e434495641d20f5a59924510
Andrew H Yi <andrew.yi50@gmail.com>
2017-05-11 13:31:49 -0400

    Setup role enum; set default to type :customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511172939) do
+ActiveRecord::Schema.define(version: 20170511173111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170511172939) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "role"
+    t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
