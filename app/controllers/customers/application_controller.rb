4e7d9dd8a12bfa427a2fceeb8f5427373cf4d1e7
Benjamin Yi <bentaebox@gmail.com>
2017-05-14 18:34:25 -0400

    Create checkout method to place order and reroute the user

--- app/controllers/customers/application_controller.rb
+++ app/controllers/customers/application_controller.rb
@@ -1,6 +1,20 @@
 module Customers
   class ApplicationController < ::ApplicationController
+
     protect_from_forgery with: :exception
+
+    def checkout
+      if current_order.place_order # returns true or false depending on whether it worked or not
+        # Delete session
+        @restaurant = current_order.restaurant
+        session.delete :order_id
+
+        render "customers/order_confirmation"
+      else
+        # TODO // Re-route to customer account management page
+        redirect_to restaurant, notice: "Insufficient funds"
+      end
+    end
 
     def example_protected_route
       render plain: "customers/application_controller#example_protected_route is working"
