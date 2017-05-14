d9122be0f51201ced1fe8583943e81e02a176709
Benjamin Yi <bentaebox@gmail.com>
2017-05-14 18:22:20 -0400

    Add balance column to users for customers and pretty string it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513034210) do
+ActiveRecord::Schema.define(version: 20170514041804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170513034210) do
     t.integer "manager_id"
     t.float "lat"
     t.float "lng"
+    t.decimal "balance_in_cents", precision: 8, scale: 2, default: "0.0"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
