8e987c787b00b9c63786729656efec422b8b9eff
Benjamin Yi <bentaebox@gmail.com>
2017-05-17 01:22:34 -0400

    Disable geolocation in test environment

--- app/models/concerns/geocodeable.rb
+++ app/models/concerns/geocodeable.rb
@@ -3,11 +3,11 @@ module Geocodeable
   extend ActiveSupport::Concern
 
   included do
-    after_validation :geocode
+    after_validation(:geocode) unless Rails.env.test?
     acts_as_mappable
     geocoded_by :address, latitude: :lat, longitude: :lng
   end
- 
+
   def to_google_maps_coords
     self.to_coordinates.join(",")
   end
