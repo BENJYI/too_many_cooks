da48f03ed5818e897c545f3c9029d1f0df532127
Benjamin Yi <bentaebox@gmail.com>
2017-05-17 15:39:45 -0400

    Fuck it commit

---
+++ db/migrate/20170517164102_add_img_url_to_menu_items.rb
@@ -0,0 +1,4 @@
+class AddImgUrlToMenuItems < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517031449) do
+ActiveRecord::Schema.define(version: 20170517164102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 20170517031449) do
     t.datetime "updated_at", null: false
     t.float "lat"
     t.float "lng"
+    t.string "img_url"
+    t.integer "rating", default: 0
     t.index ["manager_id"], name: "index_restaurants_on_manager_id"
   end
 
